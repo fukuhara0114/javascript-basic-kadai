3b05562c2c7918c00f3eae48210dfa5dea11e3ef
fukuhara0114 <fukuhara0114@gmail.com>
2024-03-19 12:54:57 +0900

    setTimeout_01

--- kadai_21/event.js
+++ kadai_21/event.js
@@ -1,1 +1,13 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', ()=>{
+
+    const text = document.getElementById('text');
+
+    setTimeout(() => {
+        
+        text.textContent = 'ボタンをクリックしました';
+    
+    }, 2000);
+});
 
