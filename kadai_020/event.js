d5cf206922a9bc145768bbaa7559e443c48f8881
fukuhara0114 <fukuhara0114@gmail.com>
2024-03-19 11:49:44 +0900

    event

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,1 +1,9 @@
+const text = document.getElementById('text');
 
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', ()=>{
+
+    text.style.fontWeight = 'bold';
+    text.textContent = 'ボタンをクリックしました';
+});
